82e3f796df6cf6b9a0070a73c2c931419b0be96d
NawarSo <sbbnawwar@gmail.com>
2025-10-31 23:28:36 +0300

    feat:implement socket.io room & namespace

--- sockets.js
+++ sockets.js
@@ -1,24 +1,29 @@
 let readyPlayerCount = 0;
 
 function listen(io) {
-    io.on('connection',(socket)=>{
+    const pongNameSpace = io.of('/pong');
+    pongNameSpace.on('connection',(socket)=>{
+        let room;
         console.log(`The user connected ${socket.id}`);
 
         socket.on('ready', () => {
-            console.log('player ready', socket.id);
+            room = 'room' + Math.floor(readyPlayerCount / 2);
+            socket.join(room);
             readyPlayerCount ++;
             if(readyPlayerCount % 2 == 0) {
-                io.emit('start', socket.id); // the seconde parameter is the referee player and in this case he is the second player
-            } 
+                pongNameSpace.in(room).emit('start', socket.id); // the seconde parameter is the referee player and in this case he is the second player
+            }
+            console.log('player ready', socket.id, room);
         });
         socket.on('paddleMove', (paddleData) => {
-            socket.broadcast.emit('paddleMove', paddleData);
+            socket.to(room).emit('paddleMove', paddleData);
         })
         socket.on('ballMove', (ballData) => {
-            socket.broadcast.emit('ballMove',ballData)
+            socket.to(room).emit('ballMove',ballData)
         });
         socket.on('disconnect', (reason) => {
             console.log(`Client ${socket.id} disconnected for ${reason} `);
+            socket.leave(room);
         });
     });
 }
